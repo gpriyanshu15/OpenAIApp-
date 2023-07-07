854b509ae4c2ab4c5740499076aa710b9e23c477
Sahil Sayyad <igsahilsayyad@gmail.com>
2023-07-08 00:14:30 +0530

    uploaded auto reply gmail bot app

---
+++ credentials.js
@@ -0,0 +1,11 @@
+
+/*
+ How this obtained ? ->see the readme.md file 
+*/
+const CLIENT_ID =
+  "648565813904-ot8e7hfm9pv5nbj395pg0q3bghdp11it.apps.googleusercontent.com";
+const CLEINT_SECRET = "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";
+const REDIRECT_URI = "https://developers.google.com/oauthplayground";
+const REFRESH_TOKEN =
+  "1//048veyFnc_5BzCgYIARAAGAQSNwF-L9IrjUA2-mRsEDJofL4y6bkkJXFqiqPKA-CONyFAK2dimczDh-ImTv04x6uMA4tHkLUTAO8";
+module.exports = { CLIENT_ID, CLEINT_SECRET, REDIRECT_URI, REFRESH_TOKEN };
